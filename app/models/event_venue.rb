c12d3104e1041eea35e15a9efd53332c5fee039a
naxolorca <filorca@miuandes.cl>
2019-04-03 09:56:06 -0300

    hi n2

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,2 +1,5 @@
 class EventVenue < ApplicationRecord
+	validates :capacity ,numericality: { only_integer: true }
+	validates :capacity ,numericality: { greater_than: 0 }
+	validates :name ,length: { minimum: 20 }
 end
