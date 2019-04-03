257b9b93286b75b690a6c04742c49ca6b888fe7c
naxolorca <filorca@miuandes.cl>
2019-04-03 11:12:09 -0300

    fished

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :tickets
+
+	validates :ticket, :presence => true 
+	
+	validate :date_ticket
+
+	def date_ticket
+	self.ticket.ticket_type.event.start_date < Date.today
+	errors.add("You can't buy a ticket when the event has started")
+	end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,14 @@
 class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
+	validates :ticket_type, :presence => true 
 	
+	validate :date_event
+
+	def date_event
+	self.ticket_type.event.start_date < Date.today
+	errors.add("The event has already started")
+	end
+
+	#La siguiente funcion esta en el modelo Order
 end
