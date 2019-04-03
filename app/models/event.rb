3534f822d32f5d06ddb04eb9436ca4f3e1f96aed
naxolorca <filorca@miuandes.cl>
2019-04-03 10:41:33 -0300

    falta Ticket

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,25 @@
 class Event < ApplicationRecord
   belongs_to :event_venue
   has_many :ticket_types
+	validates :start_date, :presence => true 
+	validate :date_event
+	validate :same_date
+
+	def date_event
+	if start_date.to_s < Date.today.to_s
+	errors.add("start_date must be later than current date of creation")
+	end
+	end
+
+	def same_date
+	events_list = Event.all
+	events_list.each{ |event| 
+	if event.event_venue_id == self.event_venue_id && event.start_date == self.start_date
+		errors.add("Two events in the same place at the same date")
+	
+	end
+	}
+	end
+	 
 end
+
